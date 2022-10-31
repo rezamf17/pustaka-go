c78a6865e4cd66536da9b3027e9cfbc0fc6a5862
reza.muhamad <reza.muhamad@pactindo.com>
2022-10-31 14:07:40 +0700

    connection to database

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"pustaka-golang/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "host=localhost user=postgres password=postgres dbname=reza-golang port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("db connection error")
+	}
+	fmt.Println("connection success")
+
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	router.GET("/", handler.Introduce)
